9c03d7eed7a3efb5adc5778a9e817f19946a807d
Mayank <mayank1365@gmail.com>
2024-05-02 00:01:34 +0530

    git init

---
+++ src/main/java/com/mayank/vehicle_service_management_backend/VehicleServiceManagementBackendApplication.java
@@ -0,0 +1,13 @@
+package com.mayank.vehicle_service_management_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleServiceManagementBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VehicleServiceManagementBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mayank/vehicle_service_management_backend/VehicleServiceManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mayank.vehicle_service_management_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleServiceManagementBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
